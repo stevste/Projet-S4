95ae201a470a0574eef67979b88a923eece024ba
Ariane <ariane.richard10@gmail.com>
2025-03-05 19:14:22 +0100

    Rotation des faces à la souris V1

--- RubiksCubeTailleN.py
+++ RubiksCubeTailleN.py
@@ -33,15 +33,12 @@ class RubiksCube:
         # Pour l'affichage des rotations :
         self.mouvementEnCours = False
         self.axeRotationEnCours = None # Axes.X, Axes.Y ou Axes.Z
-        self.sensRotationEnCours = 1
         self.abscisseFaceEnRotation:int = 0 # abscisse selon l'axe de rotation (peut être X, Y ou Z)
         self.angleRotationEnCours = 0 # en degrés
         self.configurationAnterieure = self.configuration
-
+        
         # Pour la mise en rotation avec la souris :
         self.caseCliqueeSurFaceFRONT = (None, None)
-        
-        self.listActions = [] #Actions sous la forme: (Faces, sens)
     
     def __repr__(self):
         texte = ''
@@ -58,10 +55,7 @@ class RubiksCube:
             texte += '\n'
         return texte + '----------'
     
-    def ajouterAction(self, action: tuple):
-        self.listActions.append(action)
-
-    def pivoterPlan(self, axe:int=Axes.X, abscisseFace:int=0, sens=Sens.HORAIRE) -> None:
+    def pivoterPlan(self, axe:int=Axes.X, abscisseFace:int=0, sens=Sens.HORAIRE.value) -> None:
         self.abscisseFaceEnRotation = abscisseFace
         xAParcourir = range(self.taille +2)
         yAParcourir = range(self.taille +2)
@@ -111,22 +105,16 @@ class RubiksCube:
         
         for couronne in couronnesATourner:
             if nomFace == Faces.FRONT:
-                self.sensRotationEnCours = sens.value
                 self.pivoterPlan(Axes.Y, couronne, -sens.value)
             elif nomFace == Faces.BACK:
-                self.sensRotationEnCours = -sens.value
                 self.pivoterPlan(Axes.Y, self.taille+1 - couronne, sens.value)
             elif nomFace == Faces.LEFT:
-                self.sensRotationEnCours = -sens.value
                 self.pivoterPlan(Axes.X, couronne, -sens.value)
             elif nomFace == Faces.RIGHT:
-                self.sensRotationEnCours = sens.value
                 self.pivoterPlan(Axes.X, self.taille+1 - couronne, sens.value)
             elif nomFace == Faces.DOWN:
-                self.sensRotationEnCours = -sens.value
                 self.pivoterPlan(Axes.Z, couronne, -sens.value)
-            else: # nomFace == UP:
-                self.sensRotationEnCours = sens.value
+            else: # nomFace == Faces.UP:
                 self.pivoterPlan(Axes.Z, self.taille+1 - couronne, sens.value)
     
     def coinsEtAretes(self): # avec la face verte devant et la face orange en haut
